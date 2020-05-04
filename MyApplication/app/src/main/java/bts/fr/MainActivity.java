44f67982975a82f1da210ec707c5a746e12ed7ae
NicolasDebras <nicodebras17@gmail.com>
2020-05-04 18:45:27 +0200

    Create project Android Studio

---
+++ MyApplication/app/src/main/java/bts/fr/MainActivity.java
@@ -0,0 +1,14 @@
+package bts.fr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
